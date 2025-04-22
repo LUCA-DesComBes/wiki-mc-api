065a33d312f5370010e553fff91b9ff48a6fa21f
BiscuitCuit15 <ldescombes@edenschool.fr>
2025-04-22 17:38:26 +0200

    feat (app) : finished index.html & renseignements.html & css & js of this

--- main.js
+++ main.js
@@ -1,285 +1,192 @@
-const selectClass = document.getElementById("select-class");
-const selectType = document.getElementById("select-type");
-const healthInput = document.getElementById("health");
-const ArmorInput = document.getElementById("armor");
-const DamageInput = document.getElementById("damage");
 const btnSearch = document.getElementById("btn-submit");
 let containerDivs = document.querySelector(".container-divs");
 const inputSearchs = document.querySelector(".tqt");
 const art = document.querySelector("article");
-let globalData = []
+const containerNoEntity = document.querySelector(".container-no-entity");
+const divNoEntity = document.querySelector(".div-no-entity");
+let globalData = [];
 
-function createElement(tag, attributes = {}, textContent = "") {
-	const element = document.createElement(tag);
-	for (const key in attributes) {
-		if (key === "className") element.className = attributes[key];
-		else element.setAttribute(key, attributes[key]);
-	}
-	if (textContent) element.textContent = textContent;
-	return element;
+function createElement(tag, attributes = {}, textContent = "", dataId = "") {
+  const element = document.createElement(tag);
+  for (const key in attributes) {
+    if (key === "className") element.className = attributes[key];
+    else element.setAttribute(key, attributes[key]);
+  }
+  if (textContent) element.textContent = textContent;
+  if (dataId) element.setAttribute("data-id", dataId);
+  return element;
 }
 
 function resetDivContainer() {
-	containerDivs.remove();
+  containerDivs.remove();
 
-	containerDivs = document.createElement("div");
-	containerDivs.classList.add("container-divs");
-	art.appendChild(containerDivs);
+  containerDivs = document.createElement("div");
+  containerDivs.classList.add("container-divs");
+  containerDivs.style.display = "grid";
+  containerNoEntity.style.display = "none";
+  divNoEntity.style.display = "none";
+  art.appendChild(containerDivs);
 }
 
-function createCard(name, classi, type, imgMob) {
-	const divInfo = createElement("div", { className: "div-info" });
+function createCard(name, classi, type, imgMob, dataId) {
+  const divInfo = createElement("div", { className: "div-info" });
 
-	const nameInfo = createElement("div", { className: "name-info" });
-	divInfo.appendChild(nameInfo);
+  const nameInfo = createElement("div", { className: "name-info" });
+  divInfo.appendChild(nameInfo);
 
-	const paraInfoName = createElement(
-		"p",
-		{ className: "para-info-name" },
-		name
-	);
-	nameInfo.appendChild(paraInfoName);
+  const paraInfoName = createElement(
+    "p",
+    { className: "para-info-name" },
+    name
+  );
+  nameInfo.appendChild(paraInfoName);
 
-	const imageMob = createElement("img", {
-		src: `${imgMob}`,
-		alt: `${name}`,
-		className: "imgmobschiant",
-	});
-	divInfo.appendChild(imageMob);
+  const imageMob = createElement("img", {
+    src: `${imgMob}`,
+    alt: `${name}`,
+    className: "imgmobschiant",
+  });
+  divInfo.appendChild(imageMob);
 
-	const flexRowInfo = createElement("div", { className: "flex-row-info" });
-	divInfo.appendChild(flexRowInfo);
+  const flexRowInfo = createElement("div", { className: "flex-row-info" });
+  divInfo.appendChild(flexRowInfo);
 
-	const aInfo = createElement("a", { className: "a-info" }, classi);
-	flexRowInfo.appendChild(aInfo);
+  const aInfo = createElement("a", { className: "a-info" }, classi);
+  flexRowInfo.appendChild(aInfo);
 
-	const infoCategorie = createElement("p", { className: "info-cate" }, type);
-	flexRowInfo.appendChild(infoCategorie);
+  const infoCategorie = createElement("p", { className: "info-cate" }, type);
+  flexRowInfo.appendChild(infoCategorie);
 
-	const hrInfo = createElement("hr", { className: "hr-info" });
-	divInfo.appendChild(hrInfo);
+  const hrInfo = createElement("hr", { className: "hr-info" });
+  divInfo.appendChild(hrInfo);
 
-	const btnInfo = createElement(
-		"button",
-		{ className: "btn-info", id: "toto" },
-		"SEE MORE"
-	);
+  const btnInfo = createElement(
+    "button",
+    { className: "btn-info", id: "toto" },
+    "SEE MORE",
+    dataId
+  );
 
-    btnInfo.addEventListener("click", (e) => {
-        console.log(e.target)
-    })
-	divInfo.appendChild(btnInfo);
+  btnInfo.addEventListener("click", (e) => {
+    window.location.href = `./renseignements.html?entityId=${e.target.dataset.id}`
+  });
+  divInfo.appendChild(btnInfo);
 
-	return divInfo;
+  let color = "#4F8C69";
+  if (type === "neutral") {
+    color = "#E3B599";
+  } else if (type === "hostile") {
+    color = "#D24646";
+  }
+
+  divInfo.style.border = `2px solid ${color}`;
+  nameInfo.style.backgroundColor = color;
+  btnInfo.style.backgroundColor = color;
+
+  return divInfo;
 }
 
 function displayAllCards(cards) {
-	for (let i = 0; i < cards.length; i++) {
-		const element = cards[i];
+  for (let i = 0; i < cards.length; i++) {
+    const element = cards[i];
 
-		const cardElement = createCard(
-			element.name,
-			element.classification,
-			element.type,
-			element.image
-		);
+    const cardElement = createCard(
+      element.name,
+      element.classification,
+      element.type,
+      element.image,
+      element.id
+    );
 
-		containerDivs.appendChild(cardElement);
-	}
+    containerDivs.appendChild(cardElement);
+  }
 }
 async function toto() {
-	const res = await fetch("http://51.38.232.174:3000/v1/entities");
-	const data = await res.json();
-    globalData = data;
+  containerDivs.style.display = "grid";
+  containerNoEntity.style.display = "none";
+  divNoEntity.style.display = "none";
+  const res = await fetch("http://51.38.232.174:3000/v1/entities");
+  const data = await res.json();
+  console.log(data);
+  for (const element of data) {
+    const myCard = createCard(
+      element.name,
+      element.classification,
+      element.type,
+      element.image,
+      element.id
+    );
+    containerDivs.appendChild(myCard);
+  }
 }
 toto();
 
-console.log(globalData);
-
-
-
-inputSearchs.addEventListener("input", () => {
-	btnSearch.addEventListener("click", async () => {
-		const res = await fetch(
-			`http://51.38.232.174:3000/v1/entities?name=${inputSearchs.value}`
-		);
-		const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
-
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-	});
-});
-
 btnSearch.addEventListener("click", async () => {
-const optClassi = document.getElementById("select-class");
-const optType = document.getElementById("select-type");
-      if (optClassi.value === "arthropod") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?classification=arthropod`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
+  const name = inputSearchs.value.trim();
+  const classification = document.getElementById("select-class").value;
+  const type = document.getElementById("select-type").value;
+  const healthInput = document.getElementById("health").value;
+  const armorInput = document.getElementById("armor").value;
+  const damageInput = document.getElementById("damage").value;
 
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else  if (optClassi.value === "undead") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?classification=undead`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
+  const baseURL = "http://51.38.232.174:3000/v1/entities";
+  const queryParams = [];
 
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else  if (optClassi.value === "animal") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?classification=animal`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
+  if (name) queryParams.push(`name=${name}`);
+  if (classification) queryParams.push(`classification=${classification}`);
+  if (type) queryParams.push(`type=${type}`);
+  if (healthInput) queryParams.push(`health=${healthInput}`);
+  if (armorInput) queryParams.push(`health=${armorInput}`);
+  if (damageInput) queryParams.push(`health=${damageInput}`);
 
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else  if (optClassi.value === "aquatic") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?classification=aquatic`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
+  const finalURL = `${baseURL}?${queryParams.join("&")}`;
 
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?classification=monster`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
+  try {
+    const res = await fetch(finalURL);
+    const data = await res.json();
 
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
+    resetDivContainer();
+
+    if (data.length === 0) {
+      containerDivs.style.display = "none";
+      containerNoEntity.style.display = "flex";
+      divNoEntity.style.display = "flex";
+    } else {
+      for (const element of data) {
+        const myCard = createCard(
+          element.name,
+          element.classification,
+          element.type,
+          element.image,
+          element.id
+        );
+        containerDivs.appendChild(myCard);
       }
-
-      if (optType.value === "passive") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?type=passive`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
-
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else if (optType.value === "neutral") {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?type=neutral`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
-
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      } else {
-        const res = await fetch(
-            `http://51.38.232.174:3000/v1/entities?type=hostile`
-        );
-        const data = await res.json();
-		resetDivContainer();
-		for (let i = 0; i < data.length; i++) {
-			const element = data[i];
-
-			const myCard = createCard(
-				element.name,
-				element.classification,
-				element.type,
-				element.image
-			);
-			containerDivs.appendChild(myCard);
-		}
-      }
+    }
+  } catch (error) {
+    containerDivs.style.display = "none";
+    containerNoEntity.style.display = "flex";
+    divNoEntity.style.display = "flex";
+    console.error("Erreur :", error);
+  }
 });
-
 // btnSearch.addEventListener("click", async (e) => {
 // 	e.preventDefault();
 // 	// const btnsInfo = document.getElementById("toto");
 // 	// btnsInfo.addEventListener("click", (e) => {
 // 	// 	console.log(e.target.id);
 // 	// });
-// 	// if (data.classification === "neutral") {
-// 	// 	divInfo.style.border = "2px solid #E3B599";
-// 	// 	nameInfo.style.backgroundColor = "#E3B599";
-// 	// 	btnInfo.style.backgroundColor = "#E3B599";
-// 	// } else if (data.classification === "hostile") {
-// 	// 	divInfo.style.border = "2px solid #D24646";
-// 	// 	nameInfo.style.backgroundColor = "#D24646";
-// 	// 	btnInfo.style.backgroundColor = "#D24646";
-// 	// } else {
-// 	// 	divInfo.style.border = "2px solid #4F8C69";
-// 	// 	nameInfo.style.backgroundColor = "#4F8C69";
-// 	// 	btnInfo.style.backgroundColor = "#4F8C69";
-// 	// }
+// if (data.classification === "neutral") {
+// 	divInfo.style.border = "2px solid #E3B599";
+// 	nameInfo.style.backgroundColor = "#E3B599";
+// 	btnInfo.style.backgroundColor = "#E3B599";
+// } else if (data.classification === "hostile") {
+// 	divInfo.style.border = "2px solid #D24646";
+// 	nameInfo.style.backgroundColor = "#D24646";
+// 	btnInfo.style.backgroundColor = "#D24646";
+// } else {
+// 	divInfo.style.border = "2px solid #4F8C69";
+// 	nameInfo.style.backgroundColor = "#4F8C69";
+// 	btnInfo.style.backgroundColor = "#4F8C69";
+// }
 // });
---
+++ renseigenements.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const mobNameH2 = document.getElementById("name-mod-h2");
+  const mobName = document.getElementById("mob-name");
+  const mobImg = document.getElementById("mob-image");
+  const mobType = document.getElementById("mob-type");
+  const mobClassi = document.getElementById("mob-classi");
+  const spanNom = document.getElementById("spanNom");
+  const descMob = document.getElementById("descMob");
+  const typeMobA = document.getElementById("typeMobA");
+  const paraHealth = document.getElementById("paraHealth");
+  const paraArmor = document.getElementById("paraArmor");
+  const paraDamage = document.getElementById("paraDamage");
+  const spanHeight = document.getElementById("spanHeight");
+  const spanWidth = document.getElementById("spanWidth");
+  const iconMob = document.getElementById("iconMob");
+
+  const searchParams = new URLSearchParams(window.location.search);
+  const dataId = searchParams.get("entityId");
+  console.log(dataId);
+  
+  async function test() {
+    const res = await fetch(`http://51.38.232.174:3000/v1/entities/${dataId}`);
+    const data = await res.json();
+    console.log(data);
+    mobName.textContent = data.name;
+    mobImg.src = data.image;
+    mobType.textContent = data.type;
+    mobClassi.textContent = data.classification;
+    mobNameH2.textContent = data.name;
+    typeMobA.textContent = data.classification;
+    descMob.innerHTML = `<p>${data.description}</p>`;
+    paraHealth.textContent = data.health;
+    paraArmor.textContent = data.armor;
+    spanHeight.textContent = data.height;
+    spanWidth.textContent = data.width;
+    iconMob.src = data.icon;
+  }
+  test();
+});
